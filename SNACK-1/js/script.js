72e59a27bdf6f5b0d9f54a433755cb8a262d0910
ValerioL96 <v.lembo1396@gmail.com>
2024-05-23 15:44:46 +0200

    divided array into objects with specific assigned place

--- SNACK-1/js/script.js
+++ SNACK-1/js/script.js
@@ -1,18 +1,75 @@
 //1) Crero un array per gli invitati, denominata guestList;
 //[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
   
-    const guestList = [
-        'Brad Pitt',
-        'Johnny Depp', 
-        'Lady Gaga', 
-        'Cristiano Ronaldo', 
-        'Georgina Rodriguez', 
-        'Chiara Ferragni',  
-        'George Clooney', 
-        'Amal Clooney', 
-        'Fedez', 
-        'Amadeus', 
-        'Fiorello'
-    ];
+//2) all'interno dell' array creo dei oggetti letterali javascript con le specifiche di ochi invitato:
 
-    console.log(guestList);
+ const guestList = [
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Brad Pitt',
+        place:'0'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Johnny Depp',
+        place:'1'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Lady Gaga',
+        place:'2'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Cristiano Ronaldo',
+        place:'3'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Georgina Rodriguez',
+        place:'4'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Chiara Ferragni',
+        place:'5'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'George Clooney',
+        place:'6'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Amal Clooney',
+        place:'7'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Fedez',
+        place:'8'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Amadeus',
+        place:'9'
+        
+    },
+    {
+        tableName:'Tavolo Vip',
+        guestName:'Fiorello',
+        place:'10'
+        
+    }
+   ];
+
+   console.log(guestList);
