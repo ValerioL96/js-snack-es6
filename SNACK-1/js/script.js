4b9ba45bec7d10384fea14ec2b308c86a90fed1e
ValerioL96 <v.lembo1396@gmail.com>
2024-05-23 15:40:54 +0200

    Added array for guest list

---
+++ SNACK-1/js/script.js
@@ -0,0 +1,18 @@
+//1) Crero un array per gli invitati, denominata guestList;
+//[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+  
+    const guestList = [
+        'Brad Pitt',
+        'Johnny Depp', 
+        'Lady Gaga', 
+        'Cristiano Ronaldo', 
+        'Georgina Rodriguez', 
+        'Chiara Ferragni',  
+        'George Clooney', 
+        'Amal Clooney', 
+        'Fedez', 
+        'Amadeus', 
+        'Fiorello'
+    ];
+
+    console.log(guestList);
