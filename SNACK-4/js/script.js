d18bdc9ff49ccd09c3a3f017f91c33fad479323d
ValerioL96 <v.lembo1396@gmail.com>
2024-05-30 17:00:36 +0200

    Added in Javascript array with list football teams

---
+++ SNACK-4/js/script.js
@@ -0,0 +1,34 @@
+//1) creo un array per le squadre con i nomi, punti fatti e falli subiti:
+
+ const footballTeams =[
+    {
+        name:'Tortuga',
+        points:'0',
+        foulsImmediately: '0',
+    },
+      {
+        name:'Babbagià',
+        points:'0',
+        foulsImmediately: '0',
+    },
+      {
+        name:'Milanto',
+        points:'0',
+        foulsImmediately: '0',
+    },
+      {
+        name:'Romanello',
+        points:'0',
+        foulsImmediately: '0',
+    },
+      {
+        name:'Napulegno',
+        points:'0',
+        foulsImmediately: '0',
+    },
+      {
+        name:'Vermouth',
+        points:'0',
+        foulsImmediately: '0',
+    },
+]
